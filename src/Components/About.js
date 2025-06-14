93bc7a3193c5b66bd74f37dccbd4ba17200ad638
Kyle Mair <kylemair@pm.me>
2025-06-14 19:30:25 +1000

    dir update

---
+++ src/Components/About.js
@@ -0,0 +1,16 @@
+// src/components/About.js
+import React from 'react';
+import './About.css';
+
+const About = () => {
+  return (
+    <section id="about" className="about">
+      <h2>About Me</h2>
+      <p>
+        Hi, I'm [Your Name], a passionate web developer. I love building modern, responsive websites using React, JavaScript, and other exciting technologies.
+      </p>
+    </section>
+  );
+};
+
+export default About;
